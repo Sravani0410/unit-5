6be5f84c98342ac2e2ae58f82ecf25e48e796d68
Sravani0410 <sandhyapyla.8@gmail.com>
2022-04-12 08:30:34 +0530

    second commit

--- webpackTODO/src/index.js
+++ webpackTODO/src/index.js
@@ -1,17 +1,7 @@
-import {add,mult} from "./calc"
-import "./index.css";
-import image from "./webpack.png"
-import logo from "./images/React-icon.svg.png"
-
-console.log(add(1,3))
-console.log(mult(2,3))
-
-//
-
-
-const container=document.getElementById("container");
-const img=document.createElement("img")
-//img.src=image
-img.src=logo
-img.setAttribute("class","images")
-container.append(img)
+document.querySelector("button").addEventListener("click",webpackTodo)
+function webpackTodo(){
+  let inputvalue=document.querySelector("input").value
+  let output=document.createElement("li")
+  output.textContent=inputvalue;
+  document.querySelector("ol").append(output)
+}

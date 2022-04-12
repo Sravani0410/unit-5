47c6a31c641dbdebaf7e9ad1a582b14945bffbbc
Sravani0410 <sandhyapyla.8@gmail.com>
2022-04-12 09:12:30 +0530

    fourth commit

--- webpackTODO/src/index.js
+++ webpackTODO/src/index.js
@@ -1,3 +1,16 @@
+
+import "./index.css";
+//import image from "./webpack.png"
+import logo from "./images/Simplenote_Todo.png"
+
+
+const navbar=document.getElementById("Navbar");
+const img=document.createElement("img")
+//img.src=image
+img.src=logo
+img.setAttribute("class","images")
+navbar.append(img)
+
 document.querySelector("button").addEventListener("click",webpackTodo)
 function webpackTodo(){
   let inputvalue=document.querySelector("input").value
@@ -5,3 +18,4 @@ function webpackTodo(){
   output.textContent=inputvalue;
   document.querySelector("ol").append(output)
 }
+//document.querySelector("Navbar").append(image,btn)
